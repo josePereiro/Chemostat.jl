653876ca2cbaf20700046424f5e574306ef09a05
josePereiro <jose.pereiro.lab@gmail.com>
2019-11-03 18:00:14 -0500

    Add more description to show_exchanges

--- src/ExpRess/ExpRessTools/shows.jl
+++ src/ExpRess/ExpRessTools/shows.jl
@@ -5,17 +5,52 @@ function show_exchanges(expres, ξ::Float64)
 
     mets = Chemostat.METS.exchangeable();
 
-    println("Comsuptions")
+    println("___________________________");
+    println("Comsuptions");
+    println("Met_id/ flux/ eV = min(V, c / ξ)");
     for met in sort(METS.ids(Chemostat.METS.consumable()))
         u = Chemostat.ExpRess.get_u(ξ, met);
-        u > 0.0 && println("$(met): ", u);
+        c = METS.get_c(met);
+        V = METS.get_V(met);
+        eV = Chemostat.FBA.chemostat_bound(V, c, ξ);
+        if u > 0.0
+            if isapprox(u,eV)
+                println("$(met) --------------- BOUNDED");
+                println("   u: ", u);
+                println("   c: ", c);
+                println("   V: ", V);
+                println("   eV: ", eV);
+            else
+                println("$(met) -----------------------");
+                println("   u: ", u);
+                println("   c: ", c);
+                println("   V: ", V);
+                println("   eV: ", eV);
+            end
+        end
     end
 
     println();
-    println("Secretions")
+    println("___________________________");
+    println("Secretions");
+    println("Met_id/ flux/ L");
     for met in sort(METS.ids(Chemostat.METS.secretable()))
         u = Chemostat.ExpRess.get_u(ξ, met);
-        u < 0.0 && println("$(met): ", u);
+        c = METS.get_c(met);
+        L = METS.get_L(met);
+        if u < 0.0
+            if isapprox(u,L)
+                println("$(met) --------------- BOUNDED");
+                println("   u: ", u);
+                println("   c: ", c);
+                println("   L: ", L);
+            else
+                println("$(met) -----------------------");
+                println("   u: ", u);
+                println("   c: ", c);
+                println("   L: ", L);
+            end
+        end
     end
 
     Chemostat.GEMTools.Store.restore_working_gem!();
