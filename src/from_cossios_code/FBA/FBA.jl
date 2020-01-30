1db6b57f6f7caaaac701972aa194dafa0ae32d5b
josePereiro <jose.pereiro.lab@gmail.com>
2020-01-30 14:50:48 -0500

    Add some doc

--- src/from_cossios_code/FBA/FBA.jl
+++ src/from_cossios_code/FBA/FBA.jl
@@ -1,3 +1,44 @@
+"""
+    This module performs Flux Balance Analysis (FBA)
+    assembled to the Chemostat model 
+    
+    The metabolic network has this layout:
+    
+    m: metabolites
+    n: reactions
+    S: stoichiometric matrix (m,n)
+    mets: metabolites parameters. It is a DataFrame with m rows containing:
+        id: metabolite identifier
+        c: the metabolite concentration in the feed medium
+        y: the metabolite coefficient in the objective reactions (biomass)
+        e: the metabolite coefficient in the maintenance demand reaction
+        L: the lower bound of the exchange reaction
+        V: the upper bound of the exchange reaction  
+        (you can add as many other fields as you want)
+
+    rxns: reactions parameters. It is a DataFrame with n rows containing:
+        id: reactions identifier
+        an: cost associated with the backward (negative) reaction
+        ap: cost associated with the forward (positive) reaction
+        lb: the lower bound of the reactions
+        ub: the upper bound of the reactions
+
+    The FBA problem has the form:
+        max μ 
+        s.t. 
+            W⋅x = [e,0] and 0 ≤ x ≤ upper.
+            x = [r+, r-, u, μ, ϕ]
+
+    the meaning of stuff:
+        u > 0 (up-taking)
+        u < 0 (excretion)
+
+        y > 0 (it is consumed in biomass)
+        y < 0 (it is produced in biomass)
+
+        e > 0 (it is consumed in demand)
+        e < 0 (it is produced in demand)
+"""
 module FBA
 
 using Chemostat;
